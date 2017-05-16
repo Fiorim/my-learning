1659a4b1fd86182a8e35dae56949bedd3e99ae7c
Eric Fiorim <ericfiorim@MacBook-Pro-de-Eric.local>
2017-05-15 23:58:41 -0300

    Firebase setup

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDdaEjzZWs974UZnQyUWer6jztNb3iktpA',
+    authDomain: 'my-learning-f8dbd.firebaseapp.com',
+    databaseURL: 'https://my-learning-f8dbd.firebaseio.com',
+    projectId: 'my-learning-f8dbd',
+    storageBucket: 'my-learning-f8dbd.appspot.com',
+    messagingSenderId: '107581994760'
+  }
 };
