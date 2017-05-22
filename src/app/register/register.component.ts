412a72c8bc598c1dd4888f90c8e0f3c388fbab01
Eric Fiorim <ericfiorim@mbp-de-eric.bs.br.bsch>
2017-05-22 10:10:14 -0300

    register a new note

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import * as moment from 'moment';
+
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  primaryTag = '';
+  secondaryTag = '';
+  message = '';
+  date: string;
+
+  constructor( private db: AngularFireDatabase) { }
 
   ngOnInit() {
+  }
+
+  registerNote(){
+    const note = {
+      primaryTag: this.primaryTag,
+      secondaryTag: this.secondaryTag,
+      message: this.message,
+      date: moment().format('DD-MM-YYYY')
+    };
+    this.db.database.ref().child('notes').push(note);
+    this.clearForm();
+  }
+
+  clearForm(){
+    this.primaryTag = '';
+    this.secondaryTag = '';
+    this.message = '';
   }
 
 }
